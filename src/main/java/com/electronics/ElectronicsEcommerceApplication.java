0616c44e53f6ffc5175f6b857f83296c4a6d99cc
ReemMohy259 <reem2002mohy@gmail.com>
2026-06-14 17:38:30 +0300

    Wrong code formatting for testing

--- src/main/java/com/electronics/ElectronicsEcommerceApplication.java
+++ src/main/java/com/electronics/ElectronicsEcommerceApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ElectronicsEcommerceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ElectronicsEcommerceApplication.class, args);
+        SpringApplication
+                .run(ElectronicsEcommerceApplication.class, args);
     }
 
 }
