0b694efdbc4e33a6c3fdff84edd8aaf277750f11
Yousef Abdallah <yousefabdallah031@gmail.com>
2026-06-18 14:19:58 +0300

    update product and category entities

--- src/main/java/com/electronics/entity/Category.java
+++ src/main/java/com/electronics/entity/Category.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "category")
+@Table(name = "category", uniqueConstraints = @UniqueConstraint(name = "uk_category_name", columnNames = "name"))
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_id_gen")
@@ -19,6 +19,6 @@ public class Category {
 
     @Size(max = 100)
     @NotNull
-    @Column(name = "name", nullable = false, length = 100)
+    @Column(name = "name", nullable = false, length = 100, unique = true)
     private String name;
 }
