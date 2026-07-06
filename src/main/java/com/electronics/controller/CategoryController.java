6a9642ae39265af163c39937e98f99e9318d97ca
Abdelrahman Emad <abdoemad552@gmail.com>
2026-07-07 00:12:46 +0300

    Updated realm config and added delete category

--- src/main/java/com/electronics/controller/CategoryController.java
+++ src/main/java/com/electronics/controller/CategoryController.java
@@ -6,14 +6,7 @@ import com.electronics.service.CategoryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,4 +34,8 @@ public class CategoryController {
         return categoryService.create(request);
     }
 
+    @DeleteMapping("/{id}")
+    public void remove(@PathVariable Integer id) {
+        categoryService.delete(id);
+    }
 }
